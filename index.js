b938021609a27ef9a5b5590aceb5df8249f89166
rajib074 <rajib01711173241@gmail.com>
2022-05-30 17:43:27 +0600

    Tools API Update

--- index.js
+++ index.js
@@ -1,55 +1,88 @@
-const express = require("express");
-var cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cingj0e.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cingj0e.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-async function run() {
-  try {
-    await client.connect();
-    const productCollection = client.db("data").collection("product");
-
-
-
-    app.get("/products", async (req, res) => {
-      const query = {};
-      const cursor = productCollection.find(query);
-      const products = await cursor.toArray();
-      res.send(products);
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
     });
-    app.get("/product", async (req, res) => {
-      const email = req.query.email;
-      const query = {email: email};
-      const cursor = await productCollection.find(query).toArray();
-      res.send(cursor);
-    });
-
-
-
-
-  })
-  } finally {
-  }
 }
 
+async function run() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const toolsCollection = client.db('vitac').collection('tools');
+        const reviewCollection = client.db('vitac').collection('reviews');
+        const userCollection = client.db('vitac').collection('users');
+        const orderCollection = client.db('vitac').collection('orders');
+        const paymentCollection = client.db('vitac').collection('payments');
 
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+        }
+
+        // JWT Authentication
+        app.post('/login', (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        });
+
+        // Get Tools
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const tools = await toolsCollection.find(query).toArray();
+            res.send(tools);
+        })
+
+        // Get Tools
+        app.post('/tools', async (req, res) => {
+            const tool = req.body;
+            const result = await toolsCollection.insertOne(tool);
+            res.send({ success: true });
+        })
+
+
+    }
+    finally { }
+}
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-  res.send("Hello all DJ!");
-});
+app.get('/', (req, res) => {
+    res.send('Running Server');
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Server running on port ${port}`);
+})
